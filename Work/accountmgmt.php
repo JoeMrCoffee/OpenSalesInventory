d76fb74e037c036766e9b13721361928f9566aac
Joe <45982832+JoeMrCoffee@users.noreply.github.com>
2025-10-22 18:22:09 +0800

    Adjust the spacing and login action
    Update to ensure the buttons on AccountMgmt / Account Details so they display correctly.

--- Work/accountmgmt.php
+++ Work/accountmgmt.php
@@ -17,23 +17,24 @@
 
 			echo "<h3>Account management</h3>
 				<div class='postlink'>
-				<form method='post' action='put.php' class='buttontgthr'>
+				<form method='post' action='put.php' >
 				<input type='hidden' value='$uid' name='uid'>
 				<p><strong>User name: </strong>&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='uname' value='$uname'></p>
 				<p><strong>User password: </strong>&nbsp;&nbsp;<input type='password' name='upwd' value='$upwd'></p>
 				<p><strong>User phone: </strong>&nbsp;&nbsp;&nbsp;<input type='text' name='uphone' value='$uphone'></p>
 				<p><strong>User email: </strong>&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='uemail' value='$uemail'></p>
-				<p><input type='submit' value='Update' name='accountUpdate' ></form>
+				<br><div><input type='submit' value='Update' name='accountUpdate'></form>
 				<form method='post' action='put.php' class='buttontgthr'><input type='hidden' value='$uid' name='uid'>&nbsp;&nbsp;&nbsp;
 					<input type='submit' value='Delete' name='accountDelete' onclick='return confirmfunction()'>
 				</form>
-				</p></div>";
+				</div>
+				</div>";
 		}
 	}
 	else { echo "<h3>Invalid user, please login.</h3>"; } 
 	
 	echo "<script>
-			function confirmfunction() { return confirm('Do you really want to delete this user?'); 	}
+			function confirmfunction() { return confirm('Do you really want to delete this user?'); }
 		</script>";
 	
 	include 'footer.php';
--- Work/login.php
+++ Work/login.php
@@ -3,7 +3,7 @@
 	session_destroy();
 ?>
 <h3>Login</h3>
-<form action='verify.php' method='post'>
+<form action='accountmgmt.php' method='post'>
 <input type='text' placeholder='Enter your name' name='loginName'><br><br>
 <input type='password' placeholder='Enter your password' name='password'><br><br>
 <input type='submit' value='Enter'>
